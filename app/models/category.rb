a24213e1e388805b2f35a57fc8c2138c190c2623
Anmol Kapoor <anmolk@andrew.cmu.edu>
2021-03-09 13:38:25 -0500

    Views and Controller set up for Articles

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
 	has_many :photos, :articles
 
 	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+  	scope :alphabetical, -> { order('name') }
   
 end
